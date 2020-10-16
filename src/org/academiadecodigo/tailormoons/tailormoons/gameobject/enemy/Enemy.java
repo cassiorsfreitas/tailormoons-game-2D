d90046e43bdbfe158ee96d40aedb5998e4c989dc
Code Cadet <codecadet@zxspectrum.local>
2020-10-16 14:08:16 +0100

    (Enemy) added random diection

--- src/org/academiadecodigo/tailormoons/tailormoons/gameobject/enemy/Enemy.java
+++ src/org/academiadecodigo/tailormoons/tailormoons/gameobject/enemy/Enemy.java
@@ -4,18 +4,25 @@ import org.academiadecodigo.simplegraphics.graphics.Color;
 import org.academiadecodigo.simplegraphics.graphics.Rectangle;
 import org.academiadecodigo.tailormoons.tailormoons.arena.CollisionDetector;
 import org.academiadecodigo.tailormoons.tailormoons.arena.Movable;
+import org.academiadecodigo.tailormoons.tailormoons.direction.Direction;
 import org.academiadecodigo.tailormoons.tailormoons.gameobject.GameObject;
+
+import java.util.Random;
 
 public abstract class Enemy extends GameObject implements Movable {
 
     private CollisionDetector collisionDetector;
     private EnemyType enemyType;
     private Rectangle rectangle;
+    private boolean rightDirection;
 
     public Enemy () {
         rectangle = new Rectangle(300, 500, 35, 100);
         rectangle.setColor(Color.PINK);
         display();
+
+        int randomDirection = (int) (Math.random() * 2);
+        rightDirection = randomDirection == 0;
     }
 
     public void display(){
@@ -24,6 +31,10 @@ public abstract class Enemy extends GameObject implements Movable {
 
     @Override
     public void move() {
-        rectangle.translate(1, 0);
+        if (rightDirection) {
+            rectangle.translate(1, 0);
+        } else {
+            rectangle.translate(-1, 0);
+        }
     }
 }
