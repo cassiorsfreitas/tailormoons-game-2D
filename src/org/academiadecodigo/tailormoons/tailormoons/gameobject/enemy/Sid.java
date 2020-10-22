7250fa51075a12de7fca1cc21f0a60d355177e65
cassioacademia <cassio.rsfreitas@gmail.com>
2020-10-22 18:48:20 +0100

    New images updated

--- src/org/academiadecodigo/tailormoons/tailormoons/gameobject/enemy/Sid.java
+++ src/org/academiadecodigo/tailormoons/tailormoons/gameobject/enemy/Sid.java
@@ -3,6 +3,6 @@ package org.academiadecodigo.tailormoons.tailormoons.gameobject.enemy;
 public class Sid extends Enemy {
 
     public Sid(int x, int y, int width, int height, int minMove, int maxMove) {
-        super(x, y, width, height, minMove, maxMove, "enemyRight.png", "enemyLeft.png");
+        super(x, y, width, height, minMove, maxMove, "enemyRightSid.png", "enemyLeftSid.png");
     }
 }
